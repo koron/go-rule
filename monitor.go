2f4a92c30aa6479cda3a55fb14f6241ea09eb5c2
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-10-22 00:18:41 +0900

    Monitor hooks to easily implement monitors

--- monitor.go
+++ monitor.go
@@ -10,10 +10,56 @@ type Monitor interface {
 	ActionResult(*Context, *Rule, interface{})
 }
 
-type dummyMonitor struct {}
-func (*dummyMonitor) ConditionError(*Context, *Rule, error) {}
-func (*dummyMonitor) ConditionResult(*Context, *Rule, bool) {}
-func (*dummyMonitor) ActionIgnore(*Context, *Rule) {}
+type dummyMonitor struct{}
+
+func (*dummyMonitor) ConditionError(*Context, *Rule, error)     {}
+func (*dummyMonitor) ConditionResult(*Context, *Rule, bool)     {}
+func (*dummyMonitor) ActionIgnore(*Context, *Rule)              {}
 func (*dummyMonitor) ActionCompileError(*Context, *Rule, error) {}
-func (*dummyMonitor) ActionError(*Context, *Rule, error) {}
+func (*dummyMonitor) ActionError(*Context, *Rule, error)        {}
 func (*dummyMonitor) ActionResult(*Context, *Rule, interface{}) {}
+
+type MonitorHooks struct {
+	OnConditionError     func(*Context, *Rule, error)
+	OnConditionResult    func(*Context, *Rule, bool)
+	OnActionIgnore       func(*Context, *Rule)
+	OnActionCompileError func(*Context, *Rule, error)
+	OnActionError        func(*Context, *Rule, error)
+	OnActionResult       func(*Context, *Rule, interface{})
+}
+
+func (m *MonitorHooks) ConditionError(ctx *Context, r *Rule, err error) {
+	if m.OnConditionError != nil {
+		m.OnConditionError(ctx, r, err)
+	}
+}
+
+func (m *MonitorHooks) ConditionResult(ctx *Context, r *Rule, f bool) {
+	if m.OnConditionResult != nil {
+		m.OnConditionResult(ctx, r, f)
+	}
+}
+
+func (m *MonitorHooks) ActionIgnore(ctx *Context, r *Rule) {
+	if m.OnActionIgnore != nil {
+		m.OnActionIgnore(ctx, r)
+	}
+}
+
+func (m *MonitorHooks) ActionCompileError(ctx *Context, r *Rule, err error) {
+	if m.OnActionCompileError != nil {
+		m.OnActionCompileError(ctx, r, err)
+	}
+}
+
+func (m *MonitorHooks) ActionError(ctx *Context, r *Rule, err error) {
+	if m.OnActionError != nil {
+		m.OnActionError(ctx, r, err)
+	}
+}
+
+func (m *MonitorHooks) ActionResult(ctx *Context, r *Rule, v interface{}) {
+	if m.OnActionResult != nil {
+		m.OnActionResult(ctx, r, v)
+	}
+}
